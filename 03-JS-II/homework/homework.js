63c0bb6deb5f8ef5d85ba1e8c0e09c5b9fe4650d
Sergio Orbegozo López <sergioorbegozo1@gmail.com>
2023-07-30 12:33:42 -0500

    300723

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,20 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) {
+    console.log(x);
+  } else if (y > x) {
+    console.log(y);
+  } else{
+    console.log(x || y);
+  }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  
 }
   
 function conection(status) {
